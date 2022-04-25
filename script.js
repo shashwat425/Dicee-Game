49dd7c1357f08a7a1525e09663999d907de272e7
Shashwat Jaiswal <42497807+shashwat425@users.noreply.github.com>
2022-04-25 14:57:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// # DICE 1.......
+// var randomNumber1 = Math.random();
+var randomNumber1 = Math.floor( Math.random() * 6) + 1;  //1-6
+// console.log(randomNumber1);
+
+//dice1.png - dice6.png
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+//images/dice1.png - images/dice6.png
+var randomImageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src" ,randomImageSource)
+
+
+// # DICE 2.......
+var randomNumber2 = Math.floor( Math.random() * 6) + 1;  //1-6
+// console.log(randomNumber2);
+
+//images/dice1.png - images/dice6.png
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+var image2 = document.querySelectorAll("img")[1]. setAttribute("src" ,randomImageSource2);
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML= "🚩 Player 1 Wins";
+}
+else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML= "Player 2 Wins 🚩";
+}
+else{
+    document.querySelector("h1").innerHTML= "🚩!! DRAW !! 🚩"
+}
